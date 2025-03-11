fcfa85f8203ba92a7a17746b35e32eda35242045
Pichaya D. <deachpol.p@gmail.com>
2025-03-11 20:52:30 +0700

    create main app and set theme

---
+++ lib/foodOrderScreen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FoodOrderScreen extends StatefulWidget {
+  const FoodOrderScreen({super.key});
+
+  @override
+  FoodOrderScreenState createState() => FoodOrderScreenState();
+}
